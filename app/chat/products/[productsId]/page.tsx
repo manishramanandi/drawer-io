8b8dca04885808a4c1da2e9c7405b6a2ba0acadb
Manish vaishnav <133475996+manishramanandi@users.noreply.github.com>
2025-04-05 01:16:22 +0530

    Feat: Create chat interface

---
+++ app/chat/products/[productsId]/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductId(){
+    return <h1>All products are listed here</h1>
+};
---
+++ app/chat/products/page.tsx
@@ -0,0 +1,3 @@
+export default function Products(){
+    return <h1>All products ID are listed here</h1>
+}
